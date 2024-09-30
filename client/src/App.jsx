fe4624257de479dabfcedfb2779339b4c3a03341
khalidqureshii <119107500+khalidqureshii@users.noreply.github.com>
2024-09-30 11:21:55 +0530

    Fixed Errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,9 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import Logout from './pages/Logout'
 import Home from './pages/Home'
+import PreHome from './pages/PreHome'
+import Header from './components/Header'
+import DummyHeader from './components/DummyHeader'
 
 
 function App() {
@@ -11,10 +14,11 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<><Home /></>} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/logout' element={<Logout />} />
+          <Route path='/' element={<><Header /><PreHome /></>} />
+          <Route path='/home' element={<><Header /><Home /></>} />
+          <Route path='/login' element={<><DummyHeader /><Login /></>} />
+          <Route path='/register' element={<><DummyHeader /><Register /></>} />
+          <Route path='/logout' element={<><Header /><Logout /></>} />
         </Routes>
       </BrowserRouter>
     </>
