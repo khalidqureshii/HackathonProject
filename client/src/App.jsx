57dad6cfc816782d40f6d65a6de334872f9416b1
khalidqureshii <119107500+khalidqureshii@users.noreply.github.com>
2024-09-30 10:54:18 +0530

    First Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Logout from './pages/Logout'
+import Home from './pages/Home'
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<><Home /></>} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/logout' element={<Logout />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
