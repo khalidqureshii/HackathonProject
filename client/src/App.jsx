468b0eb37479e08525a387941658afb2394a4447
Pankaj Parihar <pnkjparihar38@gmail.com>
2024-09-30 11:24:16 +0530

    Added Profile Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,32 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Logout from './pages/Logout'
-import Home from './pages/Home'
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Logout from "./pages/Logout";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<><Home /></>} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/logout' element={<Logout />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Home />
+              </>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
